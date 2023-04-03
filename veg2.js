49b6b0860367f76536207a194756020c9d74ce48
Akash Chaudhary <128380952+akash-chaudhary0201@users.noreply.github.com>
2023-04-03 11:06:38 +0530

    Add files via upload

---
+++ veg2.js
@@ -0,0 +1,28 @@
+// // function searchProducts() {
+// //     // Get the search box element
+// //     var input = document.getElementById("search-bar");
+    
+// //     // Get all product names
+// //     var productNames = document.getElementsByClassName("product-details");
+    
+// //     // Iterate through all product names and hide/show based on search term
+// //     for (var i = 0; i < productNames.length; i++) {
+// //       var productName = productNames[i].textContent.toLowerCase();
+// //       var searchTerm = input.value.toLowerCase();
+// //       var productDiv = productNames[i].closest(".product");
+      
+// //       if (productName.includes(searchTerm)) {
+// //         productDiv.style.display = "block";
+// //       } else {
+// //         productDiv.style.display = "none";
+// //       }
+// //     }
+// //   }
+
+// const resDivs = document.querySelectorAll(".res");
+
+// resDivs.forEach(resDiv => {
+//   const h2 = resDiv.querySelector("h2");
+//   const restaurantName = h2.textContent.trim();
+//   console.log(restaurantName);
+// });
